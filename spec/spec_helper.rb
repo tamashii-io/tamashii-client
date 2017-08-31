b3b8ba45ec07a3cb5e1a75da44171bc305ee7329
Liang-Chi Tseng <lctseng@5xruby.tw>
2017-08-31 10:14:09 +0000

    init commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require 'tempfile'
+require 'securerandom'
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter "/spec/"
+  track_files "lib/**/*.rb"
+end
+
+require "tamashii/client"
